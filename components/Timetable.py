e38121e75d62ca03aa312049799410b527613c75
A.R.F.R <afasihi76@gmail.com>
2022-05-29 02:37:36 +0430

    fix toggleCells of Timetable

--- components/Timetable.py
+++ components/Timetable.py
@@ -1,6 +1,22 @@
 from PyQt5 import QtCore, QtWidgets, QtGui
 from components import Settings, TableModel
 import json
+import logging
+
+
+#Creating and Configuring Logger
+Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+
+
+logging.basicConfig(filename = "logfile.log",
+                    filemode = "w",
+                    format = Log_Format, 
+                    level = logging.DEBUG,
+                    encoding='utf-8')
+
+# Logging Level = debug, info, warning, error
+
+logger = logging.getLogger()
 
 
 # Used for displaying toggable timetable
@@ -10,32 +26,52 @@ class Timetable:
         header = [['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']]
         with open('timeslots.json') as json_file:
             timeslots = json.load(json_file)['timeslots']
-        settings = Settings.getSettings()
-        header.append(timeslots[settings['starting_time']:settings['ending_time'] + 1])
+        header.append(timeslots)
         self.data = data
         if not data:
             self.data = []
-            for i in range(settings['ending_time'] + 1 - settings['starting_time']):
+            for i in timeslots:
                 self.data.append(['Available', 'Available', 'Available', 'Available', 'Available', 'Available'])
         self.model = TimetableModel(header, self.data)
         table.setModel(self.model)
         table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Fixed)
         table.verticalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Fixed)
         table.clicked.connect(self.toggleCells)
-        table.horizontalHeader().sectionClicked.connect(self.toggleCells)
-        table.verticalHeader().sectionClicked.connect(self.toggleCells)
-        table.findChild(QtWidgets.QAbstractButton).clicked.connect(self.toggleCells)
-
+        table.horizontalHeader().sectionClicked.connect(self.multiToggleCells)
+        table.verticalHeader().sectionClicked.connect(self.multiToggleCells)
+        table.findChild(QtWidgets.QAbstractButton).clicked.connect(self.multiToggleCells)
+        
+    # Toggles the availability and changes UI color to appropriate color
+    def multiToggleCells(self):
+        indexes = self.table.selectionModel().selectedIndexes()
+        counter = 0
+        for i in indexes:
+            if self.data[i.row()][i.column()] == 'Available':
+                counter += 1
+            else:
+                counter -= 1 
+        if counter > 0:
+            for i in indexes:
+                value = 'Unavailable'
+                self.table.setStyleSheet('selection-background-color: rgb(231, 76, 60); selection-color: black;')
+                self.model.setData(i, value)
+        else:
+            for i in indexes:
+                value = 'Available'
+                self.table.setStyleSheet('selection-background-color: rgb(46, 204, 113); selection-color: black;')
+                self.model.setData(i, value)
+                     
     # Toggles the availability and changes UI color to appropriate color
     def toggleCells(self):
-        indexes = self.table.selectionModel().selectedIndexes()
-        for i in indexes:
-            value = 'Available' if self.data[i.row()][i.column()] == 'Unavailable' else 'Unavailable'
-            if value == 'Available':
-                self.table.setStyleSheet('selection-background-color: rgb(46, 204, 113); selection-color: black;')
-            else:
-                self.table.setStyleSheet('selection-background-color: rgb(231, 76, 60); selection-color: black;')
-            self.model.setData(i, value)
+        indexe = self.table.selectionModel().selectedIndexes()
+        if self.data[indexe[0].row()][indexe[0].column()] == 'Unavailable': 
+            value = 'Available'
+            self.table.setStyleSheet('selection-background-color: rgb(46, 204, 113); selection-color: black;')
+        else :
+            value = 'Unavailable'
+            self.table.setStyleSheet('selection-background-color: rgb(231, 76, 60); selection-color: black;')   
+        self.model.setData(indexe[0], value)
+
 
     def getData(self):
         return self.data
