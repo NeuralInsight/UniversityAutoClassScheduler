2ea657d84469fbc6de636d342d3f08692cae248d
Seyed Ali Mojtabavi <mojtabavi99@yahoo.com>
2022-06-15 13:12:21 +0430

    change mutation and crossover model: remove sharings

--- components/GeneticAlgorithm.py
+++ components/GeneticAlgorithm.py
@@ -642,29 +642,12 @@ class GeneticAlgorithm(QtCore.QThread):
         parentA = self.chromosomes[parent[0]]
         parentB = self.chromosomes[parent[1]]
         parentAShareables = {
-            'sharings': {},
             'sections': {}
         }
-        # Parent A shall provide half of its genes
-        parentASharings = parentA.data['sharings']
-        if len(parentASharings) > 1:
-            # Amount of sharings to get
-            sharingCarve = round(len(parentASharings) / 3)
-            # Middlemost element with bias to left
-            startingPoint = int(len(parentASharings) / 2) - (sharingCarve - 1)
-            for index in range(startingPoint, startingPoint + sharingCarve):
-                # Take note that index does not mean it is the key of the sharings
-                # [{sharingId: details}]
-                sharings = [id for id in parentASharings.keys()]
-                for sharing in sharings[startingPoint:startingPoint + sharingCarve]:
-                    parentAShareables['sharings'][sharing] = parentASharings[sharing]
         # Raw list of parent A sections with reduced subjects from sharings
         parentASections = {}
         for section, value in copy.deepcopy(parentA.data['sections']).items():
             parentASections[section] = value['details']
-        for sharing in self.data['sharings'].values():
-            for section in sharing[1]:
-                parentASections[section].pop(sharing[0])
         parentASections = {key: value for key, value in filter(lambda item: len(item[1]) > 1, parentASections.items())}
         # Calculate the shareables of each section
         for section, values in parentASections.items():
@@ -678,45 +661,18 @@ class GeneticAlgorithm(QtCore.QThread):
                     parentAShareables['sections'][section] = {}
                 parentAShareables['sections'][section][subjects[index]] = values[subjects[index]]
         parentBShareables = {
-            'sharings': {},
             'sections': {}
         }
-        # Add remaining sharings from parent B
-        for id, sharing in parentB.data['sharings'].items():
-            if id not in parentAShareables['sharings'].keys():
-                parentBShareables['sharings'][id] = sharing
         # Create list of parent B sections
         parentBSections = {}
         for section, value in copy.deepcopy(parentB.data['sections']).items():
             parentBSections[section] = value['details']
-        for sharing in self.data['sharings'].values():
-            for section in sharing[1]:
-                parentBSections[section].pop(sharing[0])
         # Create list of subjects that are not in parent A shareables
         for section in parentBSections:
             parentBShareables['sections'][section] = {}
             for id, subject in parentBSections[section].items():
                 if not (id in list(parentAShareables['sections'][section].keys())):
                     parentBShareables['sections'][section][id] = subject
-        # List of unplaced sharings with or without data
-        unplacedSharings = {}
-        # Insert parent A sharings into chromosome
-        sharings = self.data['sharings']
-        for id, sharing in parentAShareables['sharings'].items():
-            if not len(sharing):
-                unplacedSharings[id] = []
-                continue
-            offspring.insertSchedule([sharing[0], sharings[id][1], sharings[id][0], sharing[1], *sharing[2:5], id])
-        # Add parent B subjects in random manner
-        parentBSharings = list(parentBShareables['sharings'].keys())
-        np.random.shuffle(parentBSharings)
-        for id in parentBSharings:
-            sharing = parentBShareables['sharings'][id]
-            if not len(sharing):
-                unplacedSharings[id] = []
-                continue
-            if offspring.insertSchedule([sharing[0], sharings[id][1], sharings[id][0], sharing[1], *sharing[2:5], id]):
-                unplacedSharings[id] = sharing
         # List of unplaced subjects with or without data
         unplacedSectionSubjects = {}
         # Insert parent A section subjects into chromosome
@@ -739,10 +695,6 @@ class GeneticAlgorithm(QtCore.QThread):
                     continue
                 if offspring.insertSchedule([details[0], [section], subject, details[1], *details[2:5]]):
                     unplacedSectionSubjects[section][subject] = details
-        # Attempt to insert unplaced sharings
-        for sharing in copy.deepcopy(unplacedSharings).keys():
-            if self.generateSubjectPlacement(sharings[sharing][1], sharings[sharing][0], sharing):
-                unplacedSharings.pop(sharing)
         # Attempt to insert unplaced section subjects
         for section, subjects in copy.deepcopy(unplacedSectionSubjects).items():
             for subject, detail in subjects.items():
@@ -751,20 +703,13 @@ class GeneticAlgorithm(QtCore.QThread):
         return offspring
 
     def mutation(self):
-        sharings = self.data['sharings']
         sections = self.data['sections']
         mutationCandidates = {
             'sections': {},
-            'sharings': [key for key in sharings.keys()]
         }
         # Prepare clean list of subject placement with consideration for sharing
         for section, data in copy.deepcopy(sections).items():
             mutationCandidates['sections'][section] = data[2]
-        for sharing in sharings.values():
-            for section in sharing[1]:
-                mutationCandidates['sections'][section].remove(sharing[0])
-        if not len(mutationCandidates['sharings']):
-            mutationCandidates.pop('sharings')
         for section in copy.deepcopy(mutationCandidates['sections']):
             if not len(mutationCandidates['sections'][section]):
                 mutationCandidates['sections'].pop(section)
@@ -775,20 +720,9 @@ class GeneticAlgorithm(QtCore.QThread):
             self.statusSignal.emit('Mutating Chromosome #{}'.format(index + 1))
             self.tempChromosome = Chromosome(self.data)
             # Select a gene to mutate
-            mutating = np.random.choice(list(mutationCandidates.keys()))
-            if mutating == 'sections':
-                section = np.random.choice(list(mutationCandidates['sections'].keys()))
-                mutating = ['sections', section, np.random.choice(mutationCandidates['sections'][section])]
-            else:
-                mutating = ['sharing', np.random.choice(mutationCandidates['sharings'])]
+            section = np.random.choice(list(mutationCandidates['sections'].keys()))
+            mutating = ['sections', section, np.random.choice(mutationCandidates['sections'][section])]
             # Replicate chromosome except the mutating gene
-            for sharing in mutationCandidates['sharings'] if 'sharings' in mutationCandidates else []:
-                if mutating[0] == 'sharing' and sharing == mutating[1]:
-                    continue
-                details = chromosome.data['sharings'][sharing]
-                if len(details):
-                    self.tempChromosome.insertSchedule(
-                        [details[0], sharings[sharing][1], sharings[sharing][0], details[1], *details[2:5], sharing])
             for section, subjects in mutationCandidates['sections'].items():
                 for subject in subjects:
                     if mutating[0] == 'sections' and mutating[1] == section and mutating[2] == subject:
@@ -797,10 +731,7 @@ class GeneticAlgorithm(QtCore.QThread):
                     if len(details):
                         self.tempChromosome.insertSchedule([details[0], [section], subject, details[1], *details[2:5]])
             # Generate mutation
-            if mutating[0] == 'sharing':
-                self.generateSubjectPlacement(sharings[mutating[1]][1], sharings[mutating[1]][0], mutating[1])
-            else:
-                self.generateSubjectPlacement([mutating[1]], mutating[2])
+            self.generateSubjectPlacement([mutating[1]], mutating[2])
             self.chromosomes[index] = copy.deepcopy(self.tempChromosome)
 
     def run(self):
